a8017588f40c6b0a8f33bb0dda312d51b0c50866
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-09-22 13:52:26 +0200

    Try 1

--- longwave.py
+++ longwave.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import math
-from methods.solar import get_solar
 import numpy as np
+from datetime import datetime
+from methods.solar import get_solar
+
+"""Model hyperparameter"""
+DT = 60 * 60  # Model time step
 
 """Physical Constants"""
 L_S = 2848 * 1000  # J/kg Sublimation
@@ -20,30 +24,81 @@ G = 9.81  # Gravitational acceleration
 
 """Surface Properties"""
 IE = 0.97  # Ice Emissivity IE
+A_I = 0.25  # Albedo of Ice A_I
+A_S = 0.85  # Albedo of Fresh Snow A_S
+A_DECAY = 16 # Albedo decay rate decay_t_d
+Z = 0.003  # Ice Momentum and Scalar roughness length
+T_PPT = 1  # Temperature condition for liquid precipitation
+# DX = 20e-03  # m Surface layer thickness growth rate
 
+H_AWS = 2
+vp_ice = (
+    np.exp(43.494- 6545.8 / (0+278))
+    / ((0+ 868)**2 * 100)
+)
 
-data = []
-p_a = 700
-RH = 50
-cld = 1
-A = 3.14 * 100
+def location_energy(p_a = 700, RH = 50, cld = 1, r = 10, v_a = 2, SW_global=1000):
 
-f = 1.0016+3.15*math.pow(10,-6) * p_a-0.074*math.pow(p_a,-1)
+    data = []
+    A = 3.14 * r**2
+    # f = 1.0016+3.15*math.pow(10,-6) * p_a-0.074*math.pow(p_a,-1)
 
-for T_a in range(-10, 10):
-    vp_a = (
-        6.107
-        * math.pow(
-            10,
-            7.5 * T_a / (T_a + 237.3),
+    for T_a in range(-10, 10):
+        vp_a = (
+            6.107
+            * math.pow(
+                10,
+                7.5 * T_a / (T_a + 237.3),
+            )
+            * RH
+            / 100
         )
-        * RH
-        / 100
-    )
-    e_a = (1.24* math.pow(abs(vp_a / (T_a + 273.15)), 1 / 7)) * (1 + 0.22 * math.pow(cld, 2))
-    LW = e_a * STEFAN_BOLTZMAN * math.pow(T_a + 273.15, 4) - IE * STEFAN_BOLTZMAN * math.pow(273.15,4)
-    ice = -LW * A/L_F * 1000/60
-    data.append([T_a, LW, round(ice,2)])
+        e_a = (1.24* math.pow(abs(vp_a / (T_a + 273.15)), 1 / 7)) * (1 + 0.22 * math.pow(cld, 2))
+        LW = e_a * STEFAN_BOLTZMAN * math.pow(T_a + 273.15, 4) - IE * STEFAN_BOLTZMAN * math.pow(273.15,4)
+        Ql = (
+            0.623
+            * L_S
+            * RHO_A
+            / P0
+            * math.pow(VAN_KARMAN, 2)
+            * v_a
+            * (vp_a - vp_ice)
+            / ((np.log(H_AWS / Z)) ** 2)
+        )
 
-df = pd.DataFrame(data, columns=['T_a','LW', 'ice'])
-print(df) 
+        Qs = (
+            C_A
+            * RHO_A
+            * p_a
+            / P0
+            * math.pow(VAN_KARMAN, 2)
+            * v_a
+            * T_a
+            / ((np.log(H_AWS / Z)) ** 2)
+        )
+
+        SW = (1 - A_I) * SW_global
+
+        ice = (Ql+Qs+LW) * A/L_F * 1000/60
+        data.append([T_a, LW, SW, Ql, Qs, round(ice,2)])
+
+    df = pd.DataFrame(data, columns=['T_a','LW', 'SW', 'Ql', 'Qs','ice'])
+    return df
+
+if __name__ == "__main__":
+
+    latitude=46.649999
+    longitude=8.283333
+    start_date=datetime(2021, 1, 1)
+    end_date=datetime(2021, 1, 31) 
+    solar_df = get_solar(
+        latitude=latitude,
+        longitude=longitude,
+        start=start_date,
+        end=end_date,
+        DT=DT,
+    )
+    print(solar_df.ghi.max())
+    df = location_energy(SW_global=solar_df.ghi.max())
+    print(df) 
+
